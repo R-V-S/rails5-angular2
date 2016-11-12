0365a4fe3a6296906a41a00841bb9a022f45bdab
Raj <rajeev.v.singh@gmail.com>
2016-11-12 08:45:58 -0500

    Replace 'it works' with Bloc branding

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Bloc Rails-Angular Starter Project';
 }
